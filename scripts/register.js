6d255a6d98c329c091deada58d30b3fbda2c30de
Davida <byourself77by@gmail.com>
2024-09-17 15:56:13 +0000

    ADDED Javacript

--- scripts/register.js
+++ scripts/register.js
@@ -1,7 +1,42 @@
 //Get form element by id
-const registerForm = document.getElementById ('register');
+const registerForm = document.querySelector("#register");
 //Handle form submit event
-registerForm.onsubmit = function(event) {
-    event.preventDefault();
-console.log(event);
-}
+registerForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+ // collect form data
+  const formData = new FormData(registerForm);
+  //send data to backend
+  //display success message
+  const messageH1 = document.querySelector("#message");
+  messageH1.textContent = "Account registered successfully!";
+});
+
+
+
+
+
+
+// registerForm.onsubmit = function (event) {
+//   event.preventDefault();
+//   console.log(event);
+// };
+ 
+
+// declare variable age
+const age = 71;
+// define square age function
+// function squareAge (age) {
+//     let newAge = age * age
+//     console.log(newAge)
+// };
+
+// squareAge(3);
+
+// // method two
+// function squareAge (age) {
+//     return age ** 2;
+// };
+
+// const squaredAge = squareAge(54);
+// console.log(squaredAge);
+
